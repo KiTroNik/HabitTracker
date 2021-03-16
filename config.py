84b17f6cacca966b775fa69d380d44ae1447c106
Jakub <kubatom3@gmail.com>
2021-03-16 19:51:52 +0100

    initial config file

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     SECRET_KEY = "\xe8\xe7\xe9\xc2T\xf5\x97\xfaTB^\xcc'O\xc8x"
-    DATABASE_URI = 'mysql://user@localhost/foo'
+    # DATABASE_URI = 'mysql://user@localhost/foo'
 
 
 class DevelopmentConfig(Config):
