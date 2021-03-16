8a468bdafa0985a541e71bf6376dbe372fd81fbf
Jakub <kubatom3@gmail.com>
2021-03-16 19:21:17 +0100

    setting up proper structure of the project

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
-from project.config import DevelopmentConfig
+from flask import Flask
+from config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+import project.views
