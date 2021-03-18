20f2f9c393e43b5af1683fe72aadf145cd493b10
Jakub <kubatom3@gmail.com>
2021-03-18 19:27:15 +0100

    Registering done

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from project.users.views import users_blueprint
 from project.habits.views import habits_blueprint
--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, session, flash
+from flask import Blueprint, render_template, redirect, url_for, session, flash, request
 from functools import wraps
 from .forms import LoginForm, RegisterForm
+from project.models import User
+from project import bcrypt, db
+from sqlalchemy.exc import IntegrityError
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -24,6 +27,20 @@ def login():
 @users_blueprint.route('/register/', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if request.method == 'POST' and form.validate():
+        user = User(
+            form.username.data,
+            form.email.data,
+            bcrypt.generate_password_hash(form.password.data)
+        )
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash('Thank you for registering')
+            return redirect(url_for('users.login'))
+        except IntegrityError:
+            flash('That username and/or email already exist.')
+            return render_template('users/register.html', form=form)
     return render_template('users/register.html', form=form)
 
 
