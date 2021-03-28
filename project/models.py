bf2266b165dc4b1a82fca9dabc22f585e8eb534c
Jakub <kubatom3@gmail.com>
2021-03-28 18:07:02 +0200

    habit model created

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from project import db
 from flask_login import UserMixin
+import datetime
 
 
 class User(UserMixin, db.Model):
@@ -9,6 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    habits = db.relationship('Habit', backref='user')
 
     def __init__(self, username=None, email=None, password=None):
         self.username = username
@@ -17,3 +19,24 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Habit(db.Model):
+    __tablename__ = "habits"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    checked = db.Column(db.Boolean, nullable=False, default=False)
+    modify_date = db.Column(db.Date, nullable=False, default=datetime.datetime.utcnow())
+    streak = db.Column(db.Integer, nullable=False, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, name, checked, modify_date, streak, user_id):
+        self.name = name
+        self.checked = checked
+        self.modify_date = modify_date
+        self.streak = streak
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'<name {self.name}'
