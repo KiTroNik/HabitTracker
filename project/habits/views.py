0b22e100b2d8764e82b566b76001c34b074633a3
Jakub <kubatom3@gmail.com>
2021-03-30 13:19:28 +0200

    habit page

--- project/habits/views.py
+++ project/habits/views.py
@@ -8,17 +8,31 @@ habits_blueprint = Blueprint('habits', __name__)
 
 
 def redefine_habits():
-    Habit.query.filter((Habit.modify_date + timedelta(days=1)) == date.today()).update(
+    Habit.query.filter((Habit.modify_date + timedelta(days=1)) == date.today())\
+        .filter(Habit.user_id == current_user.id).update(
         {Habit.checked: False, Habit.modify_date: date.today()}
     )
-    Habit.query.filter((Habit.modify_date + timedelta(days=1)) < date.today()).update(
+    Habit.query.filter((Habit.modify_date + timedelta(days=1)) < date.today())\
+        .filter(Habit.user_id == current_user.id).update(
         {Habit.checked: False, Habit.modify_date: date.today(), Habit.streak: 0}
     )
     db.session.commit()
+
+
+def checked_habits():
+    return Habit.query.filter_by(id=current_user.id).filter_by(checked=True)
+
+
+def unchecked_habits():
+    return Habit.query.filter_by(id=current_user.id).filter_by(checked=False)
 
 
 @habits_blueprint.route('/habits/')
 @login_required
 def habits():
     redefine_habits()
-    return render_template('habits/habits.html')
+    return render_template(
+        'habits/habits.html',
+        checked_habits=checked_habits(),
+        unchecked_habits=unchecked_habits()
+    )
